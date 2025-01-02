1a796493716511df1ee07b22e7692f7bee871e7d
wldy4627 <wldy4627@gmail.com>
2025-01-02 12:13:12 +0900

    다크모드 구현 완료

---
+++ script.js
@@ -0,0 +1,13 @@
+/* 다크모드 */
+const profilePhoto = document.querySelector(".profile-photo");
+
+profilePhoto.addEventListener("click", () => {
+    // if (document.body.className == 'dark-mode') {
+    //     document.body.className = '';
+    // } else {
+    //     document.body.className = 'dark-mode';
+    // }
+
+    // DOM 요소에 지정한 클래스 값이 없으면 추가하고, 있다면 제거
+    document.body.classList.toggle('dark-mode');
+})
